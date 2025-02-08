cce6fda269d83cefc5eb6f8502259cead01db4ca
Thirumalaivasan GJ <123793713+Thirumalaivasangj3@users.noreply.github.com>
2025-02-08 10:04:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = "AIzaSyCG3SbnjLcPZXQDg9KSXwVeob8dZOM9vBE"; // Your API key here
+const API_KEY = ""; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
